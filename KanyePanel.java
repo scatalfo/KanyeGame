111bd141c8f773ea1b5dee38cbfe61ed017a976a
YeezyYeezyWhatsGood <bcatalfo@gmail.com>
2017-05-22 13:35:16 -0400

    Create KanyePanel.java
    Major WIP

---
+++ KanyePanel.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class KanyePanel extends JPanel{
+
+    //construct a KanyePanel
+    public KanyePanel(){
+        //setBackground(Color.BLACK);
+    }
+
+    //paint a ball
+    public void paintComponent(Graphics g){
+      super.paintComponent(g);
+        
+    }
+
+}
